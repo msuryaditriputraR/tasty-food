84e283f098a8294843a3eaddc9f2f9cd8a785ef9
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-08-10 09:18:10 +0800

    feat: header

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+/* === SHOW MENU === */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            nav.classList.toggle("show-menu");
+        });
+    }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/* === REMOVE MENU MOBILE === */
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach((nl) =>
+    nl.addEventListener("click", () => {
+        const navMenu = document.getElementById("nav-menu");
+        navMenu.classList.remove("show-menu");
+    })
+);
+
+/* === SCROLL SECTIONS ACTIVE LINK === */
+const sections = document.querySelectorAll("section[id]");
+
+const activeLinkScroll = () => {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(`.nav__menu a[href*=${sectionId}]`)
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(`.nav__menu a[href*=${sectionId}]`)
+                .classList.remove("active-link");
+        }
+    });
+};
+
+/* === CHANGE BACKGROUND HEADER === */
+const scrollHeader = () => {
+    const header = document.getElementById("header");
+
+    header.classList[this.scrollY >= 200 ? "add" : "remove"]("scroll-header");
+};
+
+window.addEventListener("scroll", () => {
+    activeLinkScroll();
+    scrollHeader();
+});
